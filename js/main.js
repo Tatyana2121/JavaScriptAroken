888a78548e6ec936f541c98d09a10340cd1fad7b
unknown <fokus.life@gmail.com>
2025-10-04 17:35:10 +0300

    добавлено ДЗ к уроку №76

--- js/main.js
+++ js/main.js
@@ -4,83 +4,80 @@ const todoKeys = {
   id: "id",
   text: "text",
   is_completed: "is_completed",
-  date: "date"
 };
 
-const todos = [];const getNewId = (todos) =>  todos.reduce((maxId, todo) => Math.max(todo.id, maxId), 0) + 1
-  // return todos.reduce((maxId, todo) => {
-  //   if (todo.id > maxId) {
-  //     return todo.id
-  //   } else {
-  //     return maxId
-  //   }
-  // }, 0) + 1
+const todos = [];
 
-function getCurrentDateTime() {
-  const now = new Date();
-  const day = String(now.getDate());    
-  const month = String(now.getMonth() + 1); 
-  const year = now.getFullYear();
-  const hours = String(now.getHours());
-  const minutes = String(now.getMinutes());
-  return `${day}.${month}.${year} ${hours}:${minutes}`;
-}
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
 
-console.log(getCurrentDateTime());
+const getNewTodoId = (todos) =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
 
-function createTodo(todos, text) {
-  const newId = todos.length === 0 ? 1 : Math.max(...todos.map(t => t.id)) + 1;
+const createTodo = (todos, text) => {
   const newTodo = {
-    [todoKeys.id]: getNewId(todos),
+    [todoKeys.id]: getNewTodoId(todos),
     [todoKeys.text]: text,
-    [todoKeys.date]: getCurrentDateTime(),
     [todoKeys.is_completed]: false,
-  }  
-  return todos.push(newTodo)
-}
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
 
-createTodo(todos, "test1")
-createTodo(todos, "test2")
-createTodo(todos, "test3")
-console.log(todos);
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
 
-function completeTodoById(todos, todoId) {
-  const todo = todos.find((todo) => todo[todoKeys.id] === todoId)
-  if (todo === undefined) {
-    console.log(`Todo with id ${todoId} not found`);
-    return null
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
   }
   todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
-  return todo
-}
-completeTodoById(todos, 2)
-console.log(todos);
+  return todo;
+};
 
-function deleteById(todos, todoId) {  
-  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId) 
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
   if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+function editingTodoById(todos, todoId, newText) {
+  const todoIndex = todos.find((todo) => todo[todoKeys.id] === todoId);
+  if (!todoIndex) {
     console.log(`Todo with id ${todoId} not found`);
-    return todos
+    return todos;
   }
-  return todos.splice(todoIndex, 1)
+  todoIndex[todoKeys.text] = newText;
+  return todos;
 }
 
-// deleteById(todos, 2)
-// console.log(todos);
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todosList = document.querySelector(".todos");
 
-function editingTodoById (todos, todoId, newText) {
-  const todoIndex = todos.findIndex(todo => todo[todoKeys.id] === todoId)
-  if (todoId === -1) {
-        console.log(`Todo with id ${todoId} not found`);
-    return todos
-  }
-  todos.forEach((todo, index) => {
-    if (index === todoIndex)  todo[todoKeys.text] = newText    
-  });
-  return todos
+function createTodoElement(text) {
+  let createElem = document.createElement("li");
+  createElem.classList.add("todo");
+  createElem.innerHTML = `
+    <div class="todo-text">${text}</div>
+    <div class="todo-actions">
+      <button class="button-complete button">&#10004;</button>
+      <button class="button-delete button">&#10006;</button>
+    </div>
+  `;
+  todosList.append(createElem);
 }
 
-editingTodoById (todos, 1, "editing")
+function handleCreateTodo(todos, text) {
+  createTodoElement(text);
+  createTodo(todos, text);
+}
 
+handleCreateTodo(todos, "Задача 1");
+handleCreateTodo(todos, "Задача 2");
 
-
+editingTodoById(todos, 1, "editing");
+console.log(todos);
